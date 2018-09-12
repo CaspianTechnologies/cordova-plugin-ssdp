f43317910272b648335d837d23330f05b572d7cc
Roman <roman@caspiantechnologies.org>
2018-09-12 11:16:17 +0700

    Add setGoneCallback. Add device usn and name as startAdvertising arguments

--- www/ssdp.js
+++ www/ssdp.js
@@ -3,24 +3,28 @@ const device = require('cordova-plugin-device.device');
 
 exports.startSearching = function(target) {
   return new Promise(function(success, error) {
-    exec(success, error, "SSDP", "startSearching", [target])
+    exec(success, error, "SSDP", "startSearching", [target]);
   });
 }
 
 exports.startAdvertising = function(target, port) {
   return new Promise(function(success, error) {
+    const name = device.name;
     const usn = device.uuid;
-    exec(success, error, "SSDP", "startAdvertising", [target, port])
+    exec(success, error, "SSDP", "startAdvertising", [target, port, name, usn]);
   });
 }
 
 exports.stop = function() {
   return new Promise(function(success, error) {
-    const usn = device.uuid;
     exec(success, error, "SSDP", "stop", []);
   });
 }
 
 exports.setDiscoveredCallback = function(callback) {
   exec(callback, null, "SSDP", "setDiscoveredCallback", []);
+}
+
+exports.setGoneCallback = function(callback) {
+  exec(callback, null, "SSDP", "setGoneCallback", []);
 }
